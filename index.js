2a9f489407ca067a88d7d49ee72e857cd7c21603
Daniel Kalibwa <danielspecterkalibwa@gmail.com>
2019-11-05 09:57:34 +0300

    third push

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello world')
+//console.log('Hello world')
+const express = require('express');
+const app = express();
+// require is just key word used in node in order to require something
+app.listen(3000,function(){
+    console.log('listening on 3000')
+}) 
+//app.get(path,callback)
+app.get('/', function(req, res){
+    res.send('Hello world')
+})
